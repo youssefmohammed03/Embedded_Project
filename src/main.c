4f030f57517b70762ec9a70992cc708fb1e8e4ee
Youssef <21P0094@eng.asu.edu.eg>
2024-12-07 19:59:00 +0200

    .

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-#include <stdint.h>
 #include "relay.h"
 #include "DIO.h"
 
@@ -8,24 +7,35 @@ void Delay(uint32_t time) {
     }  
 }
 
-int main(void) {
-    uint8_t a = 0; // Variable initialized to 1 or 0
+uint8_t getAppButton(uint8_t appButton){
+  if(appButton){
+    return 0;
+  } else{
+    return 1;
+  }
+}
 
-    // Initialize PD2 as input
-    dio_init('D', 2, 0);
-    relay_init('D',1,1);
+// A2 for the physical button (lamp)
+// A3 for the relay (lamp)
+int main(void) {
+    uint8_t appButton = 0;
+    uint8_t physicalButton = 0;
+    uint8_t xor_result = 0;
+
+    dio_init('A', Pin2, Input);
+    relay_init('A',Pin3,Output);
 
     while (1) {
-        // Read the current state of PD2
-        uint8_t pd2_state = dio_readpin('D', 2);
+        physicalButton = dio_readpin('A', Pin2);
+        
+        appButton = getAppButton(appButton);
 
-        // Perform XOR operation between PD2 state and variable `a`
-        volatile uint8_t xor_result = pd2_state ^ a;
+        xor_result = physicalButton ^ appButton;
 
-        // Send the XOR result to control the relay lamp on PE1
-        relay_control_lamp('D',1, xor_result);
+        relay_control_lamp('A',Pin3, xor_result);
         Delay(500);
     }
 
     return 0;
 }
+
